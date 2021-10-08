b478f311aae5b7c955713c9ad33ff04083430e64
pedro-inacio11 <pedro.inacio11@gmail.com>
2021-10-08 12:29:48 -0300

    poc html converter

---
+++ src/main/java/com/example/htmlToPdf/HtmlToPdfApplication.java
@@ -0,0 +1,122 @@
+package com.example.htmlToPdf;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.util.Base64;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+@SpringBootApplication
+@RestController
+public class HtmlToPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlToPdfApplication.class, args);
+	}
+
+	@GetMapping("/genpdf/{fileName}")
+	HttpEntity<byte[]> createPdf(
+            @PathVariable("fileName") String fileName) throws IOException {
+
+		/* first, get and initialize an engine */
+		VelocityEngine ve = new VelocityEngine();
+
+		/* next, get the Template */
+		ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+		ve.setProperty("classpath.resource.loader.class",
+				ClasspathResourceLoader.class.getName());
+		ve.init();
+		Template t = ve.getTemplate("templates/helloworld.vm");
+		/* create a context and add data */
+		VelocityContext context = new VelocityContext();
+		context.put("name", fileName);
+		context.put("genDateTime", LocalDateTime.now().toString());
+		/* now render the template into a StringWriter */
+		StringWriter writer = new StringWriter();
+		t.merge(context, writer);
+		/* show the World */
+		System.out.println(writer.toString());
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+		baos = generatePdf(writer.toString());
+		String encoded = Base64.getEncoder().encodeToString(baos.toByteArray());
+		System.out.println(encoded);
+
+		HttpHeaders header = new HttpHeaders();
+	    header.setContentType(MediaType.APPLICATION_PDF);
+	    header.set(HttpHeaders.CONTENT_DISPOSITION,
+	                   "attachment; filename=" + "teste.pdf");
+	    header.setContentLength(baos.toByteArray().length);
+
+	    return new HttpEntity<byte[]>(baos.toByteArray(), header);
+
+	}
+
+	public ByteArrayOutputStream generatePdf(String html) {
+
+		String pdfFilePath = "";
+		PdfWriter pdfWriter = null;
+
+		// create a new document
+		Document document = new Document();
+		try {
+
+			document = new Document();
+			// document header attributes
+			document.addAuthor("Pedro");
+			document.addAuthor("Pedro");
+			document.addCreationDate();
+			document.addProducer();
+			document.addCreator("https://github.com/pedroinacio11");
+			document.addTitle("HTML to PDF using itext");
+			document.setPageSize(PageSize.LETTER);
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PdfWriter.getInstance(document, baos);
+
+			// open document
+			document.open();
+
+			XMLWorkerHelper xmlWorkerHelper = XMLWorkerHelper.getInstance();
+			xmlWorkerHelper.getDefaultCssResolver(true);
+			xmlWorkerHelper.parseXHtml(pdfWriter, document, new StringReader(
+					html));
+			// close the document
+			document.close();
+			System.out.println("PDF generated successfully");
+
+			return baos;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	@GetMapping("/msg")
+	public String printMesssage(){
+		return "this is the message";
+	}
+
+}
